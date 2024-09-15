0963dab390f86e3a406c028f3c2bd286c7ae59c9
Harshil <harshilpatel9912@gmail.com>
2024-09-15 14:47:13 +0530

    Committing the changes

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,20 +1,10 @@
-import React from "react";
 import { Logo } from "../icons/logo";
 import SearchBar from "@theme/SearchBar";
 
 export const NavBar = () => {
   return (
     <div
-      style={{
-        width: "100%", /* Make sure the navbar is responsive */
-        height: "100px",
-        border: "2px solid red",
-        zIndex: 1000, /* Set a lower z-index for the navbar */
-        position: "fixed", /* Keep it fixed like in your layout */
-        top: 0,
-        left: 0,
-        right: 0,
-      }}
+      style={{ width: "450px", height: "100px", border: "2px solid red" }}
       className="navContainer"
     >
       {/* <Logo /> */}
